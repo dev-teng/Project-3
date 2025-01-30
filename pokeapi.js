c0ec44c5cb282360e42319fef91e7f7fd27e05e4
dev-teng <vnctlcrn@gmail.com>
2025-01-30 10:22:06 +0800

    added result area

--- pokeapi.js
+++ pokeapi.js
@@ -1,3 +1,12 @@
 document.querySelector('#searchBtn').addEventListener('click', () => {
-  document.querySelector('#inputText').value;
-})
+const inputValue = document.querySelector('#inputText').value.toLowerCase();
+  let resultArea = document.querySelector('#results');
+  resultArea.innerHTML ="";
+
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+  .then(response => response.json())
+  .then(result => {
+    result.forEach(pokemon => console.log(pokemon.name) )
+  });
+
+});
