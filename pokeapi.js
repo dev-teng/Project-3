d72c87547849a74fcc795fa815f4b9d91303ae5e
dev-teng <vnctlcrn@gmail.com>
2025-01-30 11:11:27 +0800

    finished displaying a name of pokemon

--- pokeapi.js
+++ pokeapi.js
@@ -6,7 +6,19 @@ document.querySelector('#searchBtn').addEventListener('click', () => {
   fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
   .then(response => response.json())
   .then(result => {
-    result.results.filter(pokemon => pokemon.name.toLowerCase().includes(inputValue));
+   const filteredName = result.results.filter(pokemon => pokemon.name.toLowerCase().includes(inputValue));
+
+   filteredName.forEach((pokemon)=> {
+    let displayName = document.createElement('li');
+    displayName.textContent = pokemon.name;
+    resultArea.appendChild(displayName);
+   })
+
+   if (filteredName.length === 0) {
+    let displayName = document.createElement('li');
+    displayName.textContent = 'No pokemon found!';
+    resultArea.appendChild(displayName);
+   }
   }
   )
   .catch(error => console.log(error, 'error'));
