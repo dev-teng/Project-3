2e810af0e6dc5613b59654b2316b3afb98e22019
dev-teng <vnctlcrn@gmail.com>
2025-01-30 10:28:44 +0800

    checked if api link is working

--- pokeapi.js
+++ pokeapi.js
@@ -1,12 +1,11 @@
 document.querySelector('#searchBtn').addEventListener('click', () => {
-const inputValue = document.querySelector('#inputText').value.toLowerCase();
+  const inputValue = document.querySelector('#inputText').value.toLowerCase();
   let resultArea = document.querySelector('#results');
   resultArea.innerHTML ="";
 
   fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
   .then(response => response.json())
-  .then(result => {
-    result.forEach(pokemon => console.log(pokemon.name) )
-  });
+  .then(result=> console.log(result))
+  .catch(error => console.log(error, 'error'));
 
 });
